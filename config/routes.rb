1c21784dcadf8654b55c4d2d19bd00aa87f40865
Malak Janus <992688+malakada@users.noreply.github.com>
2024-10-12 16:48:11 -0700

    Add Devise users and copy views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
