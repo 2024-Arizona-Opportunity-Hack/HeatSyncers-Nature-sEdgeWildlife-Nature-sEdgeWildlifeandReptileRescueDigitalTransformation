570bd72bdeea1d021fc58ab0ac7ac0af16346cae
atvo912 <atvo912@gmail.com>
2024-10-13 10:32:56 -0700

    Fix revert

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
   ],
   theme: {
     colors: {
-      // primary: brandColors.navy,
+      // primary: brandColors.example,
       // secondary: brandColors.example,
       // neutral: brandColors.example,
     },
