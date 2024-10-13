f1f19b914de8c738a1a7e68701ca72ceffb3a1ae
Malak Janus <992688+malakada@users.noreply.github.com>
2024-10-13 07:35:46 -0700

    Update models and add User scope for donors

--- app/models/donor_contact.rb
+++ app/models/donor_contact.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class DonorContact < ApplicationRecord
+  belongs_to :donor, class_name: "User"
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
+
+  has_many :donor_contacts, dependent: :destroy
+  has_many :donations, dependent: :nullify
+
+  scope :donors, -> { where(is_donor: true) }
+  scope :admins, -> { where(is_admin: true) }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def address
+    "#{street_address} #{apt_suite} #{city}, #{state} #{zip}"
+  end
 end
+
